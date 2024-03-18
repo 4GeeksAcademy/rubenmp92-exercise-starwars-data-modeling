a55c190b0239f7f73a908007a3577d51def3d849
rubenmp92 <97123438+rubenmp92@users.noreply.github.com>
2024-03-18 20:37:29 +0000

    avanzando

--- src/models.py
+++ src/models.py
@@ -7,17 +7,13 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Country(Base):
-    __tablename__ = 'country'
-    ID = Column(Integer, primary_key=True)
-    name = Column(String(10))
-
 class User(Base):
     __tablename__ = 'user'
     ID = Column(Integer, primary_key=True)
-    name = Column(String(20), nullable=False)
-    pais = Column(Integer, ForeignKey('country.ID'))
-    pais_relationship = relationship(Country)
+    name = Column(String(20), nullable=False)  #nullable false para tener que escribirlo si o si 
+    planet = Column(String(20))
+    # pais = Column(Integer, ForeignKey('country.ID'))
+    # pais_relationship = relationship(Country)
     email = Column(String(50), unique=True)
     password = Column(String(25))
 
@@ -36,6 +32,21 @@ class FavoriteCharacters(Base):
     character_id = Column(Integer, ForeignKey('character.ID'))
     character_relationship = relationship(Character)
 
+class Planets(Base):
+    __tablename__= 'planets'
+    ID = Column(Integer, primary_key=True)
+    name = Column(String(20))
+    volume = Column(Integer)
+
+class FavouritePlanets(Base):
+    __tablename__= 'favorite_planets'
+    ID = Column(Integer, primary_key=True)
+    user_ID = Column
+
+# class Country(Base):
+#     __tablename__ = 'country'
+#     ID = Column(Integer, primary_key=True)
+#     name = Column(String(10))
 
 # class Person(Base):
 #     __tablename__ = 'person'
